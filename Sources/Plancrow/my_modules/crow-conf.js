05d5b94fcd312034b0ff05622b4cbe97d7253164
badmotherfucker <ilya.pimenov@gmail.com>
2013-05-19 01:50:46 +0200

    Sync

--- Sources/Plancrow/my_modules/crow-conf.js
+++ Sources/Plancrow/my_modules/crow-conf.js
@@ -5,11 +5,12 @@
  * @returns {string}
  */
 exports.mysqlConnectionString = function(){
-    var result = 'mysql://sa:assa@localhost/Plancrow?';
+    var result = 'mysql://sa:assa@localhost/Plancrow';
     if (process.env.VCAP_SERVICES !== undefined){
         var env = JSON.parse(process.env.VCAP_SERVICES);
         var cre = env['mysql-5.1'][0]['credentials'];
-        result = 'mysql://' + cre.username + ':' + cre.password + '@' + cre.hostname + '/' + cre.name + '?';
+        result = 'mysql://' + cre.username + ':' + cre.password + '@' + cre.hostname + '/' + cre.name + '';
     }
+    console.error(result);
     return result;
 }
--- Sources/Plancrow/routes/screens.js
+++ Sources/Plancrow/routes/screens.js
@@ -5,6 +5,9 @@
 
 exports.screen09_project_list = function(req, res){
     req.models.project.find({ }, function (err, projects) {
+        if (projects == undefined) {
+            projects = new Array()
+        }
         res.render('screens/09_project_list', { title: 'Plancrow', screen_name: '09 Project List', projects: projects});
     });
 };
