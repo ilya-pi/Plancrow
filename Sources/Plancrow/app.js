3b63a2031256a1cbe26f54e6068b714c025eadbd
badmotherfucker <ilya.pimenov@gmail.com>
2013-05-19 15:11:42 +0200

    Introduced Project Details page

--- Sources/Plancrow/app.js
+++ Sources/Plancrow/app.js
@@ -17,9 +17,34 @@ var app = express();
 app.use(orm.express(conf.mysqlConnectionString(), {
     define: function (db, models) {
         models.project = db.define("PROJECT", {
-            name    : String,
+            id: Number,
+            name: String,
             is_active: Boolean,
-            estimate : Number
+            estimate: Number
+        });
+
+        models.project_phase = db.define("PROJECT_PHASE", {
+            id: Number,
+            amnd_date: Date,
+            amnd_user: Number,
+            project_id: Number,
+            name: String,
+            notes: String
+        });
+
+        models.project_phase.topPhases = function (projectId, callback) {
+            this.find({ project_id: projectId, parent_id: null }, callback);
+        };
+
+        models.project_phase.phases = function (projectId, callback) {
+            this.find({ project_id: projectId}, callback);
+        };
+
+        models.project_phase.hasOne("project", models.project);
+        models.project.hasMany("phases", models.project_phase);
+
+        models.project_phase.hasOne("parent", models.project_phase, {
+            reverse: "children"
         });
     }
 }));
@@ -46,6 +71,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/screens/09_project_list', screens.screen09_project_list);
+app.get('/screens/12_project_details', screens.screen12_project_details);
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
--- Sources/Plancrow/routes/screens.js
+++ Sources/Plancrow/routes/screens.js
@@ -1,13 +1,41 @@
-
 /*
  * GET screen 09 projets list.
  */
 
-exports.screen09_project_list = function(req, res){
+exports.screen09_project_list = function (req, res) {
     req.models.project.find({ }, function (err, projects) {
         if (projects == undefined) {
             projects = new Array()
         }
         res.render('screens/09_project_list', { title: 'Plancrow', screen_name: '09 Project List', projects: projects});
+    });
+};
+
+var tree_from_list = function(list) {
+    var map = new Array()
+    var result = new Array()
+    for (i in list){
+        list[i].children = undefined
+    }
+    for (i in list){
+        var node = list[i]
+        if (node.parent_id == null){
+            result.push(node);
+            map[node.id] = node;
+        }else {
+            if (map[node.parent_id].children == undefined){
+                map[node.parent_id].children = new Array()
+            }
+            map[node.parent_id].children.push(node)
+        }
+    }
+    return result;
+}
+
+exports.screen12_project_details = function (req, res) {
+    req.models.project.get(1, function (err, project) {
+        req.models.project_phase.phases(project.id, function (err, phases) {
+            res.render('screens/12_project_details', { title: 'Plancrow', screen_name: '12 Project Details', project: project, phases: tree_from_list(phases)});
+        });
     });
 };
