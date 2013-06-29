8ebd7a1a2952b38ae3a6d4442ccc1b478c4e1df7
Ilya Pimenov <ilya.pimenov@gmail.com>
2013-06-29 12:42:53 +0200

    Implemented sign up with Passport

--- Sources/Plancrow/app.js
+++ Sources/Plancrow/app.js
@@ -9,14 +9,50 @@ var express = require('express')
     , http = require('http')
     , path = require('path')
     , conf = require('./my_modules/crow-conf.js')
-    , crowapi= require('./my_modules/crow-api.js')
-    , orm = require('orm');
+    , crowapi = require('./my_modules/crow-api.js')
+    , orm = require('orm')
+    , passport = require('passport')
+    , LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
+
+//move to separate module
+orm.connect(conf.mysqlConnectionString(), function (err, db) {
+    if (err) throw err;
+AppUser = db.define("APP_USER", {
+    id: Number,
+    login: String,
+    pwd: String,
+    first_name: String,
+    second_name: String,
+    salutation: String,
+    created_at: Date,
+    primary_email: String,
+    is_active: String
+}, {
+    methods: {
+        validPassword: function (password) {
+            return (this.pwd === password);
+        }
+    }});
+});
 
 //orm
 app.use(orm.express(conf.mysqlConnectionString(), {
     define: function (db, models) {
+        models.appUser = db.define("APP_USER", {
+            id: Number,
+            login: String,
+            pwd: Boolean,
+            first_name: String,
+            last_name: String,
+            second_name: String,
+            salutation: String,
+            created_at: Date,
+            primary_email: String,
+            is_active: String
+        });
+
         models.project = db.define("PROJECT", {
             id: Number,
             name: String,
@@ -78,6 +114,41 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+        AppUser.find({login: username }, function (err, user) {
+            if (err) {
+                return done(err);
+            }
+            if (user.length > 1){
+                return done(null, false, { message: 'Inconsistent data.' });
+            }
+            if (!user[0]) {
+                return done(null, false, { message: 'Incorrect username.' });
+            }
+            if (!user[0].validPassword(password)) {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+            return done(null, user[0]);
+        });
+    }
+));
+
+passport.serializeUser(function(user, done) {
+//    console.log(user)
+//    console.log(user.id)
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    AppUser.get(id, function(err, user) {
+        done(err, user);
+    });
+});
+
 app.use(app.router);
 app.use(require('less-middleware')({ src: __dirname + '/public' }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -93,6 +164,15 @@ app.get('/pages/01_welcome_page', screens.screen01_welcome_page);
 app.get('/pages/02_pricing_page', screens.screen02_pricing_page);
 app.get('/pages/03_registration_page', screens.screen03_registration_page);
 app.get('/pages/04_sign_in_page', screens.screen04_sign_in_page);
+
+app.post('/pages/04_sign_in_page',
+    passport.authenticate('local', { successRedirect: '/pages/09_project_list',
+        failureRedirect: '/pages/04_sign_in_page'
+//        ,
+//        failureFlash: 'Invalid username or password.'
+    })
+);
+
 app.get('/pages/05_company_admin', screens.screen05_company_admin);
 app.get('/pages/06_user_settings', screens.screen06_user_settings);
 app.get('/pages/07_user_list', screens.screen07_user_list);
