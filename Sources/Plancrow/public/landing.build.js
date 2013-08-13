bd13ff8cc724121767669a8be7e762d9107ab33a
Ilya Pimenov <ilya.pimenov@gmail.com>
2013-08-13 23:13:03 +0200

    Compressed all js files and uglified into a single one with RequireJS

---
+++ Sources/Plancrow/public/landing.build.js
@@ -0,0 +1,12 @@
+({
+    dir: "../public_compiled_packed_js",
+    paths: {
+        "jquery": "../../../public/scripts/vendor/jquery/jquery",
+        "underscore": "../../../public/scripts/vendor/underscore-amd/underscore",
+        "backbone": "../../../public/scripts/vendor/backbone-amd/backbone",
+        "bootstrap": "../../../public/scripts/vendor/bootstrap/dist/js/bootstrap.min",
+        'all_landing': "LandingApp"
+    },
+    mainConfigFile: 'scripts/client/LandingApp.js',
+    name: 'all_landing'
+})
